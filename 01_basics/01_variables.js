a91b42a052381f48f84154981241bbe32a492506
Agamjot Bedi <111156687+agxmbedi@users.noreply.github.com>
2024-07-10 08:28:21 +0000

    variable types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId =2413
+let accountEmail="agaam@gmail.com"
+var accountPassword="2413"
+accountCity = "Aps"
+let accountState;
+
+//accountId=2 
+// this is not allowed
+accountEmail="bedi@gmail.com"
+accountPassword="2413"
+accountCity="Chd"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+prefer not to use var
+always use let becuase of block scope issue and functional scope
+*/
