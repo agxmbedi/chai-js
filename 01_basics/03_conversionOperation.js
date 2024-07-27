8f0bae279e5e9d90ddedc9886dcbd6f96f5458a7
Agamjot Bedi <111156687+agxmbedi@users.noreply.github.com>
2024-07-27 20:09:51 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score=101
+console.log( typeof score);
+console.log(typeof(score));
+
+let value=Number(score);
+console.log( typeof value);
+
+// 33= > "33"
+// "112abc33" => NaN
+// true => 1, false=>0
+
+let find=1;
+let booleanFind= Boolean(find)
+console.log(booleanFind);
+// "" => false
+
