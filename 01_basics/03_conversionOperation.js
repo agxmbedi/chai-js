76b21ddf0b00608f3c758a256958c5bf274e38a8
Agamjot Bedi <111156687+agxmbedi@users.noreply.github.com>
2024-07-29 21:25:39 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,16 +1,23 @@
-let score=101
-console.log( typeof score);
-console.log(typeof(score));
+// let score=101
+// console.log( typeof score);
+// console.log(typeof(score));
 
-let value=Number(score);
-console.log( typeof value);
+// let value=Number(score);
+// console.log( typeof value);
 
-// 33= > "33"
-// "112abc33" => NaN
-// true => 1, false=>0
+// // 33= > "33"
+// // "112abc33" => NaN
+// // true => 1, false=>0
 
-let find=1;
-let booleanFind= Boolean(find)
-console.log(booleanFind);
-// "" => false
+// let find=1;
+// let booleanFind= Boolean(find)
+// console.log(booleanFind);
+// // "" => false
 
+let val=3
+let negval=-val
+console.log(negval)
+let str1="hello"
+let str2="there"
+let str3=str1+str2
+console.log(str3)
