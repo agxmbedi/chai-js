74541149f1337f4421041e873063a2a47e63f60b
Agamjot Bedi <111156687+agxmbedi@users.noreply.github.com>
2024-07-30 13:26:04 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,42 @@
+//singleton
+//onject literals
+const user={
+    name:"agambedi",
+    age:22,
+    location:"chd"
+}
+//2 ways to print from objects
+
+console.log(user.name)
+console.log(user["name"]) //good practice ,isko use karo 
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
