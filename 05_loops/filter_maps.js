607334fb5f51fa318edab100c4b55694d3ff6d93
Agamjot Bedi <111156687+agxmbedi@users.noreply.github.com>
2024-08-01 10:53:27 +0000

    Filter_maps

--- 05_loops/filter_maps.js
+++ 05_loops/filter_maps.js
@@ -1,8 +1,39 @@
-const coding=["js","java","cpp"]
+// const coding=["js","java","cpp"]
 
-const val=coding.forEach((item)=>{
-    console.log(item);
-    return;
-})
-console.log(val);
+// const val=coding.forEach((item)=>{
+//     console.log(item);
+//     return;
+// })
+//console.log(val);
 // for each meh value return nahi hoti , naho krta hai value return.
+// const nums=[1,2,3,4,5,6,7,8,9,10]
+// const value=nums.filter((num)=> {
+//     return num > 4
+    
+// }) 
+// this returns the values
+// const  mynums=[]
+// nums.forEach((num)=>{
+//     if(num> 4){
+//         mynums.push(num)
+//     }
+// })
+
+// console.log(mynums)
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  let userbook=books.filter((bk)=> bk.genre==="History")
+   userbook=books.filter((bk)=> bk.publish >= 2000)
+
+  console.log(userbook);
