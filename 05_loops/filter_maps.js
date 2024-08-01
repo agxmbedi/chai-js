1ee3d1ec9ee227e128d8e93a7f3e21077c06beca
Agamjot Bedi <111156687+agxmbedi@users.noreply.github.com>
2024-08-01 10:36:04 +0000

    filter

---
+++ 05_loops/filter_maps.js
@@ -0,0 +1,8 @@
+const coding=["js","java","cpp"]
+
+const val=coding.forEach((item)=>{
+    console.log(item);
+    return;
+})
+console.log(val);
+// for each meh value return nahi hoti , naho krta hai value return.
